dc30d2445c177b7ffe4f572be05c9fd5c1d74408
Chaitanya J. Ajmera <cjajmera007@gmail.com>
2021-09-20 16:59:38 +0530

    initial commit

---
+++ client/src/components/Landing/Landing.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import DemoButton from "./DemoButton";
+import Icon from "../Layout/Icon";
+import NavBar from "../Layout/NavBar";
+
+const Landing = (props) => {
+  return (
+    <React.Fragment>
+      <NavBar
+        isLoggedIn={props.isLoggedIn}
+        checkLogin={props.checkLogin}
+        onLogout={props.onLogout}
+      />
+      <div className="container-fluid bg">
+        <div className="" style={{ textAlign: "center", marginTop: "10vh" }}>
+          <Icon size="240px" />
+        </div>
+        <div className="jumbo">Quiz Builder</div>
+        <div className=" jumbo-subtitle">
+          Get a quiz code to join
+        </div>
+        {/* <div className="mt-5" style={{ textAlign: "center" }}>
+          <DemoButton />
+        </div> */}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Landing;
